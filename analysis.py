14507cbbe252812a0e0e0504348c4f86873639c1
Chad O'ntyamba <contyamba@gmail.com>
2026-06-08 15:07:26 +0200

    V1 Unoptimised

--- analysis.py
+++ analysis.py
@@ -3,13 +3,12 @@ from Bio.Seq import Seq
 from cleaner import detect_type
 import pandas as pd
 
-def analyze_amino(df):
+def analyze_amino(df, perc, min_amino_length=50, tb=1):
     #percentage of unusual atg to sequence length (this can be changed with your discretion)
-    percent = 3
     concentrated_atg = []
 
     #pcr = Protein coding regions: this is a df that only includes the protein coding regions
-    pcr = df[((df["stop codons"] / df["atg"]) < 2) & (df["atg_to_length(%)"] > percent)]
+    pcr = df[((df["stop codons"] / df["atg"]) < 2) & (df["atg_to_length(%)"] > perc)]
 
     #Raw DNA strand for processing
     raw_seq = pcr.loc[:, "sequence"]
@@ -18,39 +17,56 @@ def analyze_amino(df):
     #The translate method adds a * by the stop codon which we can use split to get the protein
     #trimmed the sequence to be a multiple of 3 (codon length)
 
-    type = detect_type(df)
-    print(type)
-    try:
-        if type == "DNA":
-            for x in raw_seq:
-                concentrated_atg.extend(str(Seq(x).translate()).split("*"))
-        elif type == "RNA":
-            for x in raw_seq:
-                concentrated_atg.extend(str(Seq(x).back_transcribe().translate()).split("*"))
-        elif type == "Protein":
-            for x in raw_seq:
-                concentrated_atg.extend(str(x).split("*"))
-        else: raise ValueError
-    except ValueError:
-        return "problem with format type:"
-    
-
-
-
-
-    #Now we filter out for actual protein, which must start with Methionine(M)
-    #count to label the proteins
+    #type = detect_type(df)
+    #print(type)
+    # Initialize a list to hold the mapped data instead of a flat list
+    extracted_proteins = []
     count = 0
-    c_genes = pd.Series([])
-    for x in concentrated_atg:
-        if x.startswith("M"):
-            c_genes[f"Proteins_{count}"] = x
-            count += 1
-            print(x)
+
+    molecule_type = detect_type(df)
+
+    # .items() yields BOTH the original pandas index and the sequence string
+    for original_index, seq_value in raw_seq.items():
+        
+        # 1. Translate and split based on molecule type
+        if molecule_type == "DNA":
+            chunks = str(Seq(seq_value).translate()).split("*")
+        elif molecule_type == "RNA":
+            chunks = str(Seq(seq_value).back_transcribe().translate(table=tb)).split("*")
+        elif molecule_type == "Protein":
+            chunks = str(seq_value).split("*")
+        else: 
+            raise ValueError("problem with format type:")
+
+        # 2. Immediately filter for Methionine while we still know the original_index
+        for chunk in chunks:
+            if chunk.startswith("M"):
+                amino_length = len(chunk)
+
+                if chunk.startswith("M") and amino_length >= min_amino_length:
+                    extracted_proteins.append({
+                        "original_index": original_index,
+                        "protein_label": f"Proteins_{count}",
+                        "sequence": chunk,
+                        "length": amino_length
+                    })
+                    count += 1
+                    
+
+    # 3. Convert the structured list back into a Pandas DataFrame
+    c_genes_df = pd.DataFrame(extracted_proteins)
+    if c_genes_df.empty:
+        return pd.DataFrame(columns=["Amino Acid", "Concentration (%)"]), c_genes_df
+
+    # 4. Set the index of this new table to perfectly match your main Python series
+    if not c_genes_df.empty:
+        c_genes_df.set_index("original_index", inplace=True)
+    
+    print(c_genes_df)
     #Next we take all the proteins and join them into a coherent string 
     #We break up element into a python list
     #finally we use turn it into a pd.Series so that we can calculate the percetage using value_counts
-    aa_percentages = pd.Series(list(c_genes.str.cat())).value_counts(normalize=True) * 100
+    aa_percentages = pd.Series(list(c_genes_df["sequence"].str.cat())).value_counts(normalize=True) * 100
 
     #Cleaning the DataFrame
     final_composition_df = aa_percentages.reset_index()
@@ -59,7 +75,4 @@ def analyze_amino(df):
 
     # Round the numbers so it looks clean for the README screenshot
     final_composition_df["Concentration (%)"] = final_composition_df["Concentration (%)"].round(2)
-    return final_composition_df
-
-    print("AMINO ACID COMPOSITION:")
-    final_composition_df.head(15)
+    return final_composition_df, c_genes_df
--- main.py
+++ main.py
@@ -1,25 +1,104 @@
 import streamlit as st
 from processor import process
 from cleaner import clean
+from io import BytesIO
 import analysis
 
 if "counter" not in st.session_state:
     st.session_state.counter = 0
     st.session_state.files = ""
+    st.session_state.min_amino_length = 50
+    st.session_state.start_codon_threshold = 2.8
+    st.session_state.clean_df = ""
+    st.session_state.translation = 1
+
+
+def control(step):
+    st.session_state.counter = step
+
+@st.cache_data(show_spinner=False)
+def get_clean_df(file_obj):
+    v = clean(process(file_obj))
+    return v
 
 
 def main():
-    st.title("CO Automation FASTA PROCESSOR")
+    st.set_page_config(page_title="FASTA Processor", layout="wide")
+    st.sidebar.title("Parameters")
+    st.sidebar.divider()
+    st.sidebar.number_input("Min protein length", value=50, key="min_amino_length")
+    st.sidebar.divider()
+    st.sidebar.slider("Start codon threshold(%)", value=3, key="start_codon_threshold")
+    st.sidebar.divider()
+    
+    Translation_t = {
+    "Standard (Table 1)": 1,
+    "Bacterial/Archaeal (Table 11)": 11,
+    "Mitochondrial (Table 2)": 2,
+    "Yeast Mitochondrial (Table 3)": 3
+    }
+    st.session_state.translation = st.sidebar.selectbox("Translation table", options=list(Translation_t.keys()))
+
+
     if st.session_state.counter == 0:
+        st.title("🧬 CO Automation FASTA PROCESSOR")
+        st.markdown("Automated sequence profiling, translation, and biophysical analysis.")
+
+
         st.header("Upload file below")
         st.session_state.files = st.file_uploader("Insert",type=[".fasta",".fa",".fas",".fna"])
+        st.button("Process", on_click=control, args=(1,))
+
+
+    with st.spinner("Processing data... Please wait."):
     
-    if st.session_state.files != None:
-        df = clean(process(st.session_state.files))
-        amino = analysis.analyze_amino(df=df)
-        st.write(df)
-        st.write(amino)
-        st.write(df.columns)
+        if st.session_state.files is not None and st.session_state.counter == 1:
+            df = get_clean_df(st.session_state.files)
+
+            
+            try:
+                amino, proteins = analysis.analyze_amino(df=df,
+                perc=st.session_state.start_codon_threshold,
+                min_amino_length=st.session_state.min_amino_length,
+                tb=st.session_state.translation
+
+                )
+                st.success("Analysis complete!!")
+                st.metric("Longest poly-peptide chain:", value=proteins['sequence'].str.len().max())
+                st.success("Analysis successful!")
+                col1, col2, col3 = st.columns(3)
+                col1.metric("Total Sequences Parsed", value=len(df))
+                #st.write(amino.loc[amino["Concentration (%)"].idxmax()])
+                # 1. Find the index of the highest concentration
+                top_index = amino["Concentration (%)"].idxmax()
+
+                # 2. Extract the pure string and float values using that index
+                top_aa = amino.loc[top_index, "Amino Acid"]
+                top_conc = amino.loc[top_index, "Concentration (%)"]
+
+                # 3. Pass the clean variables into the Streamlit metric
+                col2.metric(label=f"Most Expressed Amino acid: {top_aa}", value=f"{top_conc:.2f}%")
+                col3.metric("Longest Polypeptide Chain", value=proteins['sequence'].str.len().max() if not proteins.empty else 0)
+
+                st.divider()
+    
+                st.subheader("Amino Acid Distribution Profile")
+                st.bar_chart(data=amino, x="Amino Acid", y="Concentration (%)", color="#4CAF50", horizontal=True)
+                st.subheader("Extracted Coding Sequences (CDS)")
+                st.dataframe(proteins, use_container_width=True)
+
+                st.download_button(
+                    label="📥 Export Protein CDS (CSV)",
+                    data=proteins.to_csv().encode('utf-8'),
+                    file_name="cleaned_proteins_report.csv",
+                    mime="text/csv",
+                    use_container_width=True
+                )
+
+            except ValueError as e:
+                st.error(str(e))
+                st.write(df)
+    
 
     
 
